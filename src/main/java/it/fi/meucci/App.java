0f58d2267c15bc3d8b902a00431b05a915718b90
lai2-01 <lai2-01@itismeucci.com>
2022-09-27 10:01:56 +0200

    main modified

--- src/main/java/it/fi/meucci/App.java
+++ src/main/java/it/fi/meucci/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ServerStr server = new ServerStr();
+        server.waiting();
+        server.comunicate();
     }
 }
